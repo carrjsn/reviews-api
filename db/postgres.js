7a9827bde13819072e7359bafa2ddc40690dc7af
Jason Carr <carrjsn@gmail.com>
2021-07-16 17:04:05 -0700

    connect server to correct postgres table - change db file name

--- db/postgres.js
+++ db/postgres.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-var connectionString = "postgres://postgres:postgres@localhost:5432/postgres";
+var connectionString = "postgres://postgres:jasoncarr@localhost:5432/sdc_reviews";
 const client = new Client({
   connectionString: connectionString
 });
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const db = require('../db/connect.js');
+const db = require('../db/postgres.js');
 const app = express();
 
 // make db queries within server
 // - - - - - -
-// db.query('SELECT * from table1', (err, res) => {
+// db.query('SELECT * FROM reviews limit 10', (err, res) => {
 //   console.log('db server log', (err, res));
 //   db.end();
 // });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}.`)
+  console.log(`server is listening on port http://localhost:${PORT}.`)
 });
