cd2f0d5c9386b5bd1bad05c398117aabeb033e6a
Jason Carr <carrjsn@gmail.com>
2021-07-23 16:12:11 -0700

    reset helpfulness/reported after each test, remove coverage files from git and  add coverage to gitignore

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = require('../server/app.js');
 const request = require('supertest');
+const db = require('../db/postgres.js');
 
+// allow long queries to be tested
 jest.setTimeout(50000);
 
 describe('Add a review', () => {
@@ -36,9 +38,10 @@ describe('Get meta data', () => {
     expect(response.statusCode).toBe(200);
   });
 
-  it('should return meta data for recommended votes', async () => {
+  it('recommended data should have true and false keys', async () => {
     const response = await request(app).get('/reviews/meta?product_id=121756');
     expect(response.body.recommended).toHaveProperty('true');
+    expect(response.body.recommended).toHaveProperty('false');
   });
 
   it('should return meta data for recommended votes', async () => {
@@ -61,9 +64,11 @@ describe('Get meta data', () => {
 describe('Update helpfulness', () => {
 
   // add beforeEach to reset helpfulness change
-  it('respond with a 204 status code', async () => {
+  it('should respond with a 204 status code', async () => {
     const response = await request(app).put('/reviews/121756/helpful');
     expect(response.statusCode).toBe(204);
+    // undo increment helpfulness
+    await db.query('UPDATE reviews SET helpfulness = helpfulness - 1 WHERE id = 121756');
   });
 
   it('should add one to the helpfulness count of a review', async () => {
@@ -75,6 +80,8 @@ describe('Update helpfulness', () => {
     const updatedResponse = await request(app).get('/reviews?product_id=123456');
     let updatedReviewHelpfulness = updatedResponse.body.results[0].helpfulness;
     expect(updatedReviewHelpfulness).toEqual(reviewHelpfulness + 1);
+    // reset count
+    await db.query(`UPDATE reviews SET helpfulness = helpfulness - 1 WHERE id = ${reviewId}`);
   });
 
 });
@@ -82,23 +89,26 @@ describe('Update helpfulness', () => {
 describe('Report a review', () => {
 
   it('respond with a 204 status code', async () => {
-    const response = await request(app).put('/reviews/121256/report');
+    const response = await request(app).put('/reviews/121252/report');
     expect(response.statusCode).toBe(204);
+    // db.query undo the report
+    await db.query('UPDATE reviews SET reported = false WHERE id = 121252');
   });
 
   it('reported review should not be rendered on subsequent fetches', async () => {
-    const response = await request(app).get('/reviews?product_id=121256');
+    const response = await request(app).get('/reviews?product_id=121259');
     let reviewId = response.body.results[0].review_id;
-
+    console.log(reviewId, 'reviewid');
     //report review
     await request(app).put(`/reviews/${reviewId}/report`);
 
-    const updatedResponse = await request(app).get('/reviews?product_id=121256');
+    const updatedResponse = await request(app).get('/reviews?product_id=121259');
     const updatedReviews = updatedResponse.body.results;
     updatedReviews.forEach((review) => {
       expect(review.id).not.toEqual(reviewId);
-    })
-
+    });
+    // db.query undo the report - after each?
+    await db.query(`UPDATE reviews SET reported = false WHERE id = ${reviewId}`);
   });
 
 });
